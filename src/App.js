c04dc8f85baf9b5a13bdff520ac2623653b58d39
HyesuChoi <chlgotn0580@naver.com>
2023-05-31 22:18:08 +0900

    music app
    spotify clone

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
+import { Login } from "./components/Login";
+import React, { useEffect } from "react";
+import { useStateProvider } from "./utils/StateProvider";
+import { reducerCases } from "./utils/Constants";
+import { Spotify } from "./components/Spotify";
 
 function App() {
-  return <div className="App">App</div>;
+  const [{ token }, dispatch] = useStateProvider();
+  useEffect(() => {
+    const hash = window.location.hash;
+    if (hash) {
+      const token = hash.substring(1).split("&")[0].split("=")[1];
+      dispatch({ type: reducerCases.SET_TOKEN, token });
+    }
+  }, [token, dispatch]);
+
+  return <div className="App">{token ? <Spotify /> : <Login />}</div>;
 }
 
 export default App;
